1f0afb803dbc4b97b48d1b9f8652f53d485e24d3
Jayashan Liyanage <laksrijayashan@gmail.com>
2025-11-07 22:16:39 +0530

    chatgpt task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CounterComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ng-try';
+  parentCount: number = 5;
+
 }
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+@Input() count: number = 0;
+@Output() countChange = new EventEmitter<number>();
+
+  increment() {
+    this.count++;
+    this.countChange.emit(this.count);
+  }
+
+  decrement() {
+    this.count--;
+    this.countChange.emit(this.count);
+  }
+}
